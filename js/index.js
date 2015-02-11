b425930f6d71924857a5c3db821281b0772c300f
Ming Horn <minghaihorn@gmail.com>
2015-02-11 10:21:45 -0800

    oauth madness

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
-OAuth.initialize("tUYjyhX06oJCVPXNVEwwJDZxg");
+OAuth.initialize('KKm7o9nR2-5SDvDAdXwUyBp27NE');
+OAuth.popup('twitter', {cache: true}).done(function(twitter) {
+  //make API calls with `twitter`
+}).fail(function(err) {
+  //todo when the OAuth flow failed
+})
